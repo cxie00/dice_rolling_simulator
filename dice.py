91e290be1036d32e90cbd4a8180c9356b9db6fea
C. Xie <32278569+cxie00@users.noreply.github.com>
2021-02-18 18:16:17 -0500

    Add files via upload

--- dice.py
+++ dice.py
@@ -17,15 +17,19 @@ class DiceSimulator():
         min = 1
         max = num_sides
 
-        rolls = []
+        # we use a loop because we want to do the same action (throw a die) multiple times
         for i in range (1, num_die + 1):
             roll_value = random.randint(min, max)
             print("Roll {}: {}".format(i, roll_value))
             self.sum += roll_value
 
+        # print the sum of the rolls we threw
         print("Sum: {}".format(results.sum))
 
+        # calculate average 
         average = self.getAvg(num_die)
+
+        # print average
         print("Average: {}".format(average))
         return self.sum
     
@@ -41,19 +45,25 @@ class DiceSimulator():
 if __name__ == '__main__':
     game_running = True
 
+    # main game loop
     while (game_running): 
+
         print("Enter number of dice you want to throw: ")
         num_die = input()
+
         print("Enter the number of sides on the die you want to use (e.g. 3, 4, ... 20)")
         num_sides = input()
 
         print("We will roll {}d{}.".format(num_die, num_sides))
 
+        # instantiate new DiceSimulator object
         results = DiceSimulator()
         results.rollDie(int(num_die), int(num_sides))
 
         print("Roll again? Y/N")
         answer = input()
+
+
         if (answer.lower() == 'n'):
             game_running = False
     print("Bye! Thanks for playing Dice Simulator.")
